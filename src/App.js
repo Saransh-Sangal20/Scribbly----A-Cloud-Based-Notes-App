cdc062b1879a74fc36abd4b9348291b45b64b392
Saransh-Sangal20 <182532011+Saransh-Sangal20@users.noreply.github.com>
2025-10-29 02:34:18 +0530

    Login component made

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import NoteState from './context/notes/NoteState';
 import Home from './components/Home';
 import About from './components/About';
+import Signup from './components/Signup';
+import Login from './components/Login';
 
 function App() {
   return (
@@ -14,6 +16,8 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
           </Routes>
         </div>
       </Router>
---
+++ src/components/Login.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import { useState } from "react";
+
+export default function Login() {
+  const [credentials, setcredentials] = useState({email: "", password: ""});
+
+  const handleLogin = async (e) => {
+    e.preventDefault();  // prevent page from reloading on form submit
+    const response = await fetch("http://localhost:5000/api/auth/login", {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({email: credentials.email, password: credentials.password})
+    })
+    const json = await response.json();
+    console.log(json);
+  }
+  const handleChange = (e) => {
+        setcredentials({...credentials, [e.target.name]: e.target.value});
+    }
+  return (
+    <div>
+      <form onSubmit={handleLogin}>
+        <div className="mb-3">
+          <label htmlFor="email" className="form-label">Email address</label>
+          <input type="email" className="form-control" id="email" name="email" value={credentials.email} onChange={handleChange} aria-describedby="emailHelp" />
+          <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="password" className="form-label">Password</label>
+          <input type="password" className="form-control" id="password" name="password" value={credentials.password} onChange={handleChange} />
+        </div>
+        <div className="mb-3 form-check">
+          <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+          <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
+        </div>
+        <button type="submit" className="btn btn-primary">Login</button>
+      </form>
+    </div>
+  )
+}
